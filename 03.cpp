82fd778865419a9f1eb684f4e5e87fab64d56909
cheyao <94928179+cheyao@users.noreply.github.com>
2024-06-05 19:33:14 +0200

    Better day1

--- 03.cpp
+++ 03.cpp
@@ -1,88 +1,114 @@
+#include <algorithm>
 #include <cctype>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
-int Part1() {
-	std::ifstream input("03.input", std::ios::in);
-	int sum = 0;
+std::vector<std::string> file;
 
-	std::vector<std::string> schematic;
+void readFile() {
+	std::ifstream input("03.input");
+	std::string line;
 
-	while (1) {
-		std::string line;
+	if (!input.is_open()) {
+		std::cerr << "Failed to open input" << std::endl;
+		exit(1);
+	}
+
+	while (!input.eof()) {
 		std::getline(input, line);
-
-		if (input.eof()) {
-			break;
-		}
-
-		schematic.emplace_back(line);
+		file.emplace_back(line);
 	}
 
-	for (int y = 0; y < schematic.size(); ++y) {
-		int number = 0;
-		bool part = false;
-
-		for (int x = 0; x < schematic[0].size(); ++x) {
-			if (schematic[y][x] != '.') {
-				// Befor & after
-				if (x != 0 && schematic[y][x - 1] != '.' &&
-				    !std::isdigit(schematic[y][x - 1])) {
-					part = true;
-				}
-				if (x != schematic[0].size() - 1 &&
-				    schematic[y][x + 1] != '.' &&
-				    !std::isdigit(schematic[y][x + 1])) {
-					part = true;
-				}
-
-				// Top and bottom
-				for (int i = (x == 0 ? 0 : -1);
-				     i <=
-				     (x == schematic[0].size() - 1 ? 0 : 1);
-				     ++i) {
-					if (y != 0 &&
-					    schematic[y - 1][x + i] != '.' &&
-					    !std::isdigit(
-						schematic[y - 1][x + i])) {
-						part = true;
-					}
-
-					if (y != schematic.size() - 1 &&
-					    schematic[y + 1][x + i] != '.' &&
-					    !std::isdigit(
-						schematic[y + 1][x + i])) {
-						part = true;
-					}
-				}
-			}
-
-			if (std::isdigit(schematic[y][x])) {
-				number *= 10;
-				number += schematic[y][x] - '0';
-
-				if ((x == schematic[0].size() - 1) && part) {
-					sum += number;
-					number = 0;
-					part = false;
-				}
-			} else if (part) {
-				sum += number;
-				number = 0;
-				part = false;
-			} else {
-				number = 0;
-			}
-		}
-	}
-
-	return sum;
+	// There will be a blank line at the end
+	file.pop_back();
 }
 
-int main(int argc, char** argv) {
-	std::cout << "[Part 1] The sum is " << Part1() << std::endl;
+void part1() {
+	const int line_size = file[0].size();
+	std::unordered_map<int, int*> number_map;
 
-	return 0;
+	int* tmp = new int;
+	*tmp = 0;
+	for (int y = 0; y < file.size(); ++y) {
+		for (int x = 0; x < line_size; ++x) {
+			char c = file[y][x];
+
+			if (std::isdigit(c)) {
+				*tmp *= 10;
+				*tmp += c - '0';
+				number_map[y * line_size + x] = tmp;
+			}
+
+			if (!std::isdigit(c) && *tmp != 0) {
+				tmp = new int;
+				*tmp = 0;
+			}
+		}
+
+		if (*tmp != 0) {
+			tmp = new int;
+			*tmp = 0;
+		}
+	}
+
+	std::vector<int*> found;
+	int sum = 0;
+	// Now check for gears
+	for (int y = 0; y < file.size(); ++y) {
+		for (int x = 0; x < line_size; ++x) {
+			char c = file[y][x];
+
+			if (std::isdigit(c) || c == '.') {
+				// Ins't the symbol
+				continue;
+			}
+
+			std::vector<int> checks;
+
+			// Ahh we need to map the numbers to check
+			if (y != 0 && x != 0) 
+				checks.emplace_back((y - 1) * line_size + x - 1);
+			if (y != file.size() - 1 && x != line_size - 1) 
+				checks.emplace_back((y + 1) * line_size + x + 1);
+			if (x != line_size - 1) 
+				checks.emplace_back((y + 0) * line_size + x + 1);
+			if (y != 0) 
+				checks.emplace_back((y - 1) * line_size + x + 0);
+			if (y != 0 && x != line_size - 1) 
+				checks.emplace_back((y - 1) * line_size + x + 1);
+			if (x != 0) 
+				checks.emplace_back((y - 0) * line_size + x - 1);
+			if (y != file.size() - 1 && x != 0) 
+				checks.emplace_back((y + 1) * line_size + x - 1);
+			if (y != file.size() - 1) 
+				checks.emplace_back((y + 1) * line_size + x - 0);
+
+			// Check all boxes around
+			for (auto i : checks) {
+				if (number_map[i] == nullptr ||
+				    std::find(found.begin(), found.end(),
+					      number_map[i]) != found.end()) {
+					// Aleady added or isn't valid
+					continue;
+				}
+
+				sum += *number_map[i];
+				found.push_back(number_map[i]);
+			}
+		}
+	}
+
+	std::cout << "The sum of part 1 is: " << sum << std::endl;
+	// LOL memeory leaks!
+}
+
+int main() {
+	std::cout << "Day 3:" << std::endl;
+
+	readFile();
+
+	part1();
 }
