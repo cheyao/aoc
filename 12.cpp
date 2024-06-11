f9b3e62c99bc4bdf0a4e8c8f4157fb3ff228594c
cheyao <94928179+cheyao@users.noreply.github.com>
2024-06-11 18:22:05 +0200

    Day 12 part 1 with recursion!

--- 12.cpp
+++ 12.cpp
@@ -1,8 +1,7 @@
-#include <algorithm>
+#include <chrono>
 #include <cmath>
 #include <cstdint>
 #include <cstdlib>
-#include <deque>
 #include <fstream>
 #include <utility>
 #include <vector>
@@ -12,71 +11,52 @@
 using namespace std;
 using std::operator""sv;
 
-bool check(string s, vector<uint64_t> dst) {
-	vector<uint64_t> src;
-	bool checked;
+bool check(string& s, vector<uint64_t>& dst) {
+	vector<uint64_t> src = {};
+	bool checked = false;
+	int times = 0;
 
-	for (int i = 0; i < s.size(); i++) {
+	for (int i = 0; i < s.size(); ++i) {
 		if (s[i] == '#') {
-			if (!checked) {
-				src.emplace_back(1);
-			} else {
-				src[src.size() - 1]++;
+			++times;
+		} else {
+			if (times != 0) {
+				src.emplace_back(times);
 			}
 
-			checked = true;
-		} else {
-			checked = false;
+			times = 0;
 		}
+	}
+
+	if (times != 0) {
+		src.emplace_back(times);
 	}
 
 	return src == dst;
 }
 
-uint64_t posibility(pair<string, vector<uint64_t>> row) {
+uint64_t recursion(string s, vector<uint64_t>& rows) {
+	if (!s.contains('?')) {
+		return check(s, rows);
+	}
+
 	uint64_t count = 0;
 
-	string m = row.first;
-	vector<uint64_t>& spring = row.second;
-	vector<char*> rand;
-
-	for (int i = 0; i < m.size(); i++) {
-		if (m[i] == '?') {
-			rand.emplace_back(&(m[i]));
-		}
-	}
-
-	uint64_t pos = std::count(m.begin(), m.end(), '?');
-	uint64_t pos2 = std::pow(2, pos);
-	uint64_t mask = 0;
-	for (uint64_t i = 0; i < pos2; i++) {
-		uint64_t maskCopy = mask;
-		// Reset m
-		for (auto c : rand) {
-			*c = '?';
-		}
-
-		for (uint64_t j = 0; j < pos; j++) {
-			if ((mask >> j) & 1) {
-				*rand[j] = '#';
-			} else {
-				*rand[j] = '.';
-			}
-		}
-
-		if (check(m, spring)) {
-			count++;
-			// cout << "Check " << i << " with " << m << endl;
-		}
-
-		mask++;
-	}
+	auto cur = s.find_first_of('?');
+	s[cur] = '#';
+	count += recursion(s, rows);
+	s[cur] = '.';
+	count += recursion(s, rows);
 
 	return count;
 }
 
+uint64_t posibility(pair<string, vector<uint64_t>>& row) {
+	return recursion(row.first, row.second);
+}
+
 void part1() {
-	ifstream file("12.input");
+	ifstream file("12.input.small");
 	vector<pair<string, vector<uint64_t>>> springMap;
 
 	while(1) {
@@ -102,4 +82,46 @@ void part1() {
 	cout << "Day 12 part 1: " << sum << endl;
 }
 
-int main() { part1(); }
+void part2() {
+	ifstream file("12.input.small");
+	vector<pair<string, vector<uint64_t>>> springMap;
+
+	while(1) {
+		string line;
+		getline(file, line);
+		if (file.eof()) {
+			break;
+		}
+		string a;
+		vector<uint64_t> b;
+		a = line.substr(0, line.find(' '));
+		line.erase(0, line.find(' ') + 1);
+		b = line | views::split(","sv) | views::transform([](auto s)->uint64_t{ return atol(s.data()); }) | ranges::to<vector<uint64_t>>();
+		vector<uint64_t> out;
+		out.reserve(b.size() * 5); // preallocate memory
+		for (int i = 0; i < 5; i++) {
+			out.insert(out.end(), b.begin(), b.end());
+		}
+		springMap.emplace_back((pair<string, vector<uint64_t>>) {a+a+a+a+a, out});
+	}
+
+	// Brute force our way through
+	uint64_t sum = 0;
+	for (auto springRow : springMap) {
+		sum += posibility(springRow);
+	}
+
+	cout << "Day 12 part 2: " << sum << endl;
+}
+
+int main() { 
+	auto begin = chrono::high_resolution_clock::now();
+	part1(); 
+	auto end = chrono::high_resolution_clock::now();
+	std::cout << chrono::duration_cast<chrono::nanoseconds>(end-begin).count() << "ns" << std::endl;
+
+	auto begin2 = chrono::high_resolution_clock::now();
+
+	auto end2 = chrono::high_resolution_clock::now();
+	std::cout << chrono::duration_cast<chrono::nanoseconds>(end2-begin2).count() << "ns" << std::endl;
+}
