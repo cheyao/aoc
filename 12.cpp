ade9818b2c1f7f03e98babd34697fe3b610df887
cheyao <94928179+cheyao@users.noreply.github.com>
2024-06-12 13:26:45 +0200

    Day 12 part 1 with caching!

--- 12.cpp
+++ 12.cpp
@@ -1,8 +1,12 @@
+#include <algorithm>
 #include <chrono>
 #include <cmath>
 #include <cstdint>
+#include <cstdio>
 #include <cstdlib>
 #include <fstream>
+#include <string>
+#include <map>
 #include <utility>
 #include <vector>
 #include <ranges>
@@ -11,52 +15,74 @@
 using namespace std;
 using std::operator""sv;
 
-bool check(string& s, vector<uint64_t>& dst) {
-	vector<uint64_t> src = {};
-	bool checked = false;
-	int times = 0;
+map<pair<string, vector<uint64_t>>, uint64_t> cache;
 
-	for (int i = 0; i < s.size(); ++i) {
-		if (s[i] == '#') {
-			++times;
+// Call: ???.### 1,1,3--\
+//    1: .??.### 1,1,3  #??.### 1,1,3
+//    2:                 ??.### 1,3
+uint64_t recursion(string s, vector<uint64_t> rows, uint64_t ammount = 0) {
+	if (s.empty()) {
+		if (!rows.empty() && ammount == rows.front()) {
+			rows.erase(rows.begin());
+		}
+
+		if (rows.empty()) {
+			return 1;
 		} else {
-			if (times != 0) {
-				src.emplace_back(times);
-			}
-
-			times = 0;
+			return 0;
 		}
 	}
 
-	if (times != 0) {
-		src.emplace_back(times);
+	if (cache.contains({s, rows})) {
+		return cache[{s, rows}];
+	} 
+
+	// Isn't in cache ^n^
+	if (s[0] == '?') {
+		// Can be # and .
+		uint64_t sum = 0;
+		s[0] = '#';
+		sum += recursion(s, rows, ammount);
+		s[0] = '.';
+		sum += recursion(s, rows, ammount);
+		cache[{s, rows}] = sum;
+		return sum;
+	} 
+
+	if (s[0] == '#') {
+		ammount++;
+		if (rows.empty() || ammount > rows.front()) {
+			return 0;
+		}
+
+		s = s.substr(1);
+		return recursion(s, rows, ammount);
 	}
 
-	return src == dst;
-}
-
-uint64_t recursion(string s, vector<uint64_t>& rows) {
-	if (!s.contains('?')) {
-		return check(s, rows);
+	if (s[0] == '.') {
+		if (ammount != 0) {
+			if (ammount == rows.front()) {
+				rows.erase(rows.begin());
+			} else {
+				return 0;
+			}
+		} 
+		
+		ammount = 0;
+		s = s.substr(1);
+		return recursion(s, rows, ammount);
 	}
 
-	uint64_t count = 0;
-
-	auto cur = s.find_first_of('?');
-	s[cur] = '#';
-	count += recursion(s, rows);
-	s[cur] = '.';
-	count += recursion(s, rows);
-
-	return count;
+	exit(1); // This is not supposed to happed
+	return 0;
 }
 
-uint64_t posibility(pair<string, vector<uint64_t>>& row) {
+uint64_t posibility(pair<string, vector<uint64_t>> row) {
 	return recursion(row.first, row.second);
 }
 
 void part1() {
-	ifstream file("12.input.small");
+	ifstream file("12.input");
 	vector<pair<string, vector<uint64_t>>> springMap;
 
 	while(1) {
@@ -76,6 +102,7 @@ void part1() {
 	// Brute force our way through
 	uint64_t sum = 0;
 	for (auto springRow : springMap) {
+		cache.clear();
 		sum += posibility(springRow);
 	}
 
@@ -102,26 +129,37 @@ void part2() {
 		for (int i = 0; i < 5; i++) {
 			out.insert(out.end(), b.begin(), b.end());
 		}
-		springMap.emplace_back((pair<string, vector<uint64_t>>) {a+a+a+a+a, out});
+		springMap.emplace_back((pair<string, vector<uint64_t>>) {a+"?"+a+"?"+a+"?"+a+"?"+a, out});
 	}
 
 	// Brute force our way through
 	uint64_t sum = 0;
 	for (auto springRow : springMap) {
-		sum += posibility(springRow);
+		cache.clear();
+		// sum += posibility(springRow);
 	}
 
 	cout << "Day 12 part 2: " << sum << endl;
 }
 
+void test() {
+	if (posibility((pair<string, vector<uint64_t>>) {".#?", {1}}) == 1) {
+		cout << " Passed" << endl;
+	} else {
+		cout << " Failed" << endl;
+	}
+}
+
 int main() { 
+	// test();
+
 	auto begin = chrono::high_resolution_clock::now();
 	part1(); 
 	auto end = chrono::high_resolution_clock::now();
 	std::cout << chrono::duration_cast<chrono::nanoseconds>(end-begin).count() << "ns" << std::endl;
 
 	auto begin2 = chrono::high_resolution_clock::now();
-
+	// part2();
 	auto end2 = chrono::high_resolution_clock::now();
 	std::cout << chrono::duration_cast<chrono::nanoseconds>(end2-begin2).count() << "ns" << std::endl;
 }
