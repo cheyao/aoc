b8cecdc4798e8c32586256303f0c79e15423075e
cheyao <94928179+cheyao@users.noreply.github.com>
2024-06-13 22:03:09 +0200

    Day 16 part 1

---
+++ 16.cpp
@@ -0,0 +1,203 @@
+#include <Eigen/Dense>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+using Eigen::Vector2i;
+
+typedef Vector2i Vector2;
+
+const static Vector2 ZERO(0, 0);
+const static Vector2 NORTH(0, -1);
+const static Vector2 EAST(1, 0);
+const static Vector2 SOUTH(0, 1);
+const static Vector2 WEST(-1, 0);
+
+typedef struct Laser {
+	Laser(Vector2 pos = ZERO, Vector2 vel = ZERO) : position(pos), velocity(vel) {};
+
+	Vector2 position;
+	Vector2 velocity;
+
+	bool zero() {
+		return velocity == ZERO;
+	}
+
+	bool operator==(const Laser& laser) {
+		return (position == laser.position) && (velocity == laser.velocity);
+	}
+
+	void advance() {
+		this->position += this->velocity;
+	}
+	
+	bool inside(const vector<string>& layout) {
+		return (
+				this->position.x() >= 0 &&
+				this->position.x() < layout[0].size() &&
+				this->position.y() >= 0 &&
+				this->position.y() < layout.size()
+		       );
+	}
+} Laser;
+
+// +-----> x
+// |
+// |
+// |
+//\/ y
+
+typedef struct FootprintS {
+	uint8_t N : 1; // ^
+	uint8_t E : 1; // ->
+	uint8_t S : 1; // \/
+	uint8_t W : 1; // <-
+} FootprintS;
+
+typedef union Footprint {
+	uint8_t val = 0;
+	FootprintS bits;
+} Footprint;
+
+// Only change direction
+// Need to .advance()
+Laser bounce(Laser& laser, const char& tile) {
+	switch(tile) {
+		case '\\': {
+			int x = laser.velocity.x();
+			laser.velocity.x() = laser.velocity.y();
+			laser.velocity.y() = x;
+			break;
+		}
+
+		case '/': {
+			int x = laser.velocity.x();
+			laser.velocity.x() = -laser.velocity.y();
+			laser.velocity.y() = -x;
+			break;
+		}
+
+		case '-': {
+			if (laser.velocity.y() != 0) {
+				laser.velocity = EAST;
+
+				return Laser(laser.position, WEST); 
+			}
+			
+			break;
+		}
+
+		case '|': {
+			if (laser.velocity.x() != 0) {
+				laser.velocity = NORTH;
+
+				return Laser(laser.position, SOUTH); 
+			}
+			break;
+		}
+
+		case '.': {
+			break;
+		}
+
+		default: {
+			break;
+		}
+	}
+
+	return Laser(); // NULL
+}
+
+Footprint toBits(Laser& l) {
+	Footprint foot;
+
+	if (l.velocity == NORTH) { foot.bits.N = 1; }
+	if (l.velocity == EAST)  { foot.bits.E = 1; }
+	if (l.velocity == SOUTH) { foot.bits.S = 1; }
+	if (l.velocity == WEST)  { foot.bits.W = 1; }
+
+	return foot;
+}
+
+void shoot(queue<Laser>& lasers, const vector<string>& layout, vector<vector<Footprint>>& footprints) {
+	while (!lasers.empty()) {
+		Laser laser = lasers.front();
+		lasers.pop();
+
+		if (!laser.inside(layout)) {
+			continue;
+		}
+
+		int in = footprints[laser.position.y()][laser.position.x()].val & toBits(laser).val;
+		if (in) {
+			continue;
+		}
+		footprints[laser.position.y()][laser.position.x()].val |= toBits(laser).val;
+
+		const char& tile = layout[laser.position.y()][laser.position.x()];
+
+		Laser newLaser = bounce(laser, tile);
+
+		if (!newLaser.zero()) {
+			newLaser.advance();
+			lasers.push(newLaser);
+		}
+
+		laser.advance();
+		lasers.push(laser);
+	}
+}
+
+void part1() {
+	fstream file("16.input");
+	vector<string> layout;
+
+	// Read the layout
+	while (1) {
+		string s;
+		getline(file, s);
+
+		if (file.eof()) {
+			break;
+		}
+
+		layout.emplace_back(s);
+	}
+	
+	vector<vector<Footprint>> footprints;
+	// Initialize footprints
+	for (int i = 0; i < layout.size(); ++i) {
+		footprints.emplace_back(vector<Footprint>());
+		for (int j = 0; j < layout[0].size(); j++) {
+			footprints.back().emplace_back(Footprint());
+		}
+	}
+	// footprints[0][0].bits.E = 1;
+
+	// Walked paths
+	queue<Laser> lasers;
+	lasers.push({Laser(ZERO, EAST)});
+
+	shoot(lasers, layout, footprints);
+
+	int energized = 0;
+	for (auto line : footprints) {
+		for (auto footprint : line) {
+			if (footprint.val != 0) {
+				// cout << '#';
+				energized++;
+			} /* else {
+				cout << '.';
+			} */
+		}
+	}
+
+	cout << "Part 1: " << energized << endl;
+}
+
+int main() {
+	part1();
+}
