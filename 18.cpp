8a3d67776402ff3c17f0b769bccaffd833c99dd6
cheyao <94928179+cheyao@users.noreply.github.com>
2024-06-17 20:06:58 +0200

    18p2

--- 18.cpp
+++ 18.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 typedef struct Vector2 {
-	int x, y;
+	uint64_t x, y;
 
 	Vector2() : x(0), y(0) {};
 	Vector2(uint64_t _x, uint64_t _y) : x(_x), y(_y) {};
@@ -101,9 +101,66 @@ void part1() {
 	}
 
 	// Now count the area
-	cout << (0.5 * (S1 - S2)) + (parim  /2.+1.) << endl;
+	cout << ((S1 - S2) / 2) + (parim  / 2 + 1) << endl;
+}
+
+void part2() {
+	ifstream file("18.input");
+	uint64_t S1 = 0, S2 = 0;
+	uint64_t parim = 0;
+	Vector2 last;
+
+	while (1) {
+		string s;
+		getline(file, s);
+		if (file.eof()) {
+			break;
+		}
+		stringstream ss(s);
+
+		Vector2 dir;
+		uint64_t blocks, discardi;
+		char c, discard;
+		string color;
+		ss >> discard;
+		ss >> discardi;
+
+		ss >> color;
+		switch (color[color.size() - 2]) {
+			case '3':
+				dir = UP;
+				break;
+			case '1':
+				dir = DOWN;
+				break;
+			case '2':
+				dir = LEFT;
+				break;
+			case '0':
+				dir = RIGHT;
+				break;
+			default:
+				exit(1);
+		}
+
+		color = color.substr(2, color.size() - 4);
+		blocks = stoi(color, 0, 16);
+
+		parim += blocks;
+		dir *= blocks;
+		dir += last;
+
+		S1 += last.x * dir.y;
+		S2 += dir.x * last.y;
+
+		last = dir;
+	}
+
+	// Now count the area
+	cout << ((S1 - S2) / 2) + (parim  / 2 + 1) << endl;
 }
 
 int main() {
 	part1();
+	part2();
 }
