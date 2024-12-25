7743c98349a33073c1cfcbd1f3dda86f3ef654c2
Cyao <cyao@duck.com>
2024-12-25 16:44:42 +0000

    Website

---
+++ introduction.cpp
@@ -0,0 +1,11 @@
+#include <print>
+
+int main() {
+	std::println("Initializing program...");
+	std::println("Hello! Welcome to my AOC solutions website!");
+	std::println("Here you will see all of my AOC solutions");
+	std::println("Coded entirely in C++!");
+
+	std::println("PS. Please don't mine my html/css/js");
+	std::println("The website is a bonus, my main lang is C++");
+}
