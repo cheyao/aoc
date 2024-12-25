62e69ab6ec6e573d2b93ec5acb1091b31c5cffe6
Cyao <cyao@duck.com>
2024-12-25 19:39:36 +0000

    Readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-My AOC solutions
+# C++ advent of code solutions
 
-Planning to add this to my personal site
+- Entirely in C++
+- No external libraries
+- Uses newest C++26 features
+- Optimized, everything works under 0.5 seconds
+- All the solutions for 2024 are custom made!
+
+![](https://cloud-gtl8lxc1j-hack-club-bot.vercel.app/0image.png)
--- introduction.cpp
+++ introduction.cpp
@@ -8,11 +8,15 @@ int main() {
 	std::println("All solutions are entirely in C++!");
 
 	std::println("- Modern C++ used");
-	std::println("- Every day is <1s");
+	std::println("- Every day runs under 0.5s");
 	std::println("- Licensed under zlib");
 
-	std::println("PS. Please don't mine my html/css/js");
+	std::println("PS. Please don't mind my html/css/js");
 	std::println("The website is a bonus, my main lang is C++");
 
 	std::println("Merry Christmas!");
+
+	return 0;
 }
+
+// PS. Did you know? C++ had println now!
