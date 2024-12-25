290557ce4280238816bb327e906a93f4e5f25cf0
Cyao <cyao@duck.com>
2024-12-25 17:21:16 +0000

    Main part of website

--- introduction.cpp
+++ introduction.cpp
@@ -1,11 +1,18 @@
 #include <print>
 
+// Initializing program...
+
 int main() {
-	std::println("Initializing program...");
 	std::println("Hello! Welcome to my AOC solutions website!");
 	std::println("Here you will see all of my AOC solutions");
-	std::println("Coded entirely in C++!");
+	std::println("All solutions are entirely in C++!");
+
+	std::println("- Modern C++ used");
+	std::println("- Every day is <1s");
+	std::println("- Licensed under zlib");
 
 	std::println("PS. Please don't mine my html/css/js");
 	std::println("The website is a bonus, my main lang is C++");
+
+	std::println("Merry Christmas!");
 }
