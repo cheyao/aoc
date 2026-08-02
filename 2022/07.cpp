084bc237bda5cf959784537e833965a8d0b971ac
cheyao <cyao@duck.com>
2026-08-02 10:14:27 +0200

    Day 07 2022

--- 2022/07.cpp
+++ 2022/07.cpp
@@ -1,143 +1,112 @@
 #include <algorithm>
 #include <cassert>
-#include <cctype>
-#include <cstddef>
-#include <cstdint>
-#include <cstring>
 #include <fstream>
 #include <iostream>
-#include <iterator>
-#include <numeric>
-#include <ranges>
-#include <sstream>
-#include <stack>
 #include <string>
 #include <vector>
 
-using namespace std;
+#define INPUT "07.cpp.txt"
 
-vector<string> getFile(const char* name) {
-	vector<string> lines;
+std::vector<std::string> getInput(void) {
+	std::vector<std::string> lines;
 
-	ifstream file(name);
-	string str;
-	while (getline(file, str)) {
-		lines.emplace_back(str);
+	std::ifstream file(INPUT);
+	std::string line;
+	while (std::getline(file, line)) {
+		lines.emplace_back(line);
 	}
 	file.close();
 
 	return lines;
 }
 
-uint64_t part1(const char* file);
-uint64_t part2(const char* file);
+typedef struct file {
+	bool dir;
+	uint64_t size;
+	std::vector<struct file> children;
+} file;
+std::vector<file*> stack;
+std::vector<uint64_t> sizes;
 
-int main(int argc, char** argv) {
-	if (argc <= 1) {
-		cout << "Usage: " << argv[0] << " [FILE_NAME]" << endl;
+uint64_t sum_size(file* f) {
+	uint64_t s = 0;
 
-		return 0;
-	}
-
-	cout << "Day " << __FILE__[0] << __FILE__[1] << " part 1 : " << part1(argv[1]) << endl;
-	cout << "Day " << __FILE__[0] << __FILE__[1] << " part 2 : " << part2(argv[1]) << endl;
-}
-
-class File {
-  public:
-	File(const std::size_t size, const std::string name) : mSize(size), mName(name) {}
-
-	virtual std::size_t size() const noexcept { return mSize; }
-
-  private:
-	std::size_t mSize;
-	std::string mName;
-};
-
-class Dir : public File {
-  public:
-	Dir(const std::string name) : File(0, name) {}
-
-	std::size_t size() const noexcept override {
-		return std::accumulate(mFiles.begin(), mFiles.end(), 0,
-							   [](int x, const File* file) { return x + file->size(); });
-	}
-	void addFile(const File* file) noexcept { mFiles.emplace_back(file); }
-
-  private:
-	std::vector<const File*> mFiles;
-};
-
-uint64_t part1(const char* file) {
-	const auto& lines = getFile(file);
-
-	vector<File*> files;
-	std::string pwd = "";
-	stack<Dir*> pwdS;
-
-	for (std::size_t i = 0; i < lines.size(); ++i) {
-		const char* content = lines[i].data();
-
-		assert(content[0] == '$');
-
-		content += 2;
-
-		if (content[0] == 'c') {
-			content += 3;
-			if (content[0] == '/') {
-				files.clear();
-				pwd = "";
-
-				while (!pwdS.empty()) {
-					pwdS.pop();
-				}
-
-				continue;
-			}
-
-			if (content[0] == '.' && content[1] == '.') {
-				pwd.erase(pwd.find_last_of('/'));
-				pwdS.pop();
-
-				continue;
-			}
-
-			pwd += '/';
-			pwd += content;
-		} else {
-			assert(content[0] == 'l');
-
-			do {
-				++i;
-
-				const std::string entry = lines[i].data();
-				std::stringstream entrys(entry);
-				std::string name;
-
-				if (entry.starts_with("dir")) {
-					entrys >> name;
-					entrys >> name;
-
-					files.emplace_back(new Dir(pwd + '/' + name));
-				} else {
-					std::size_t size;
-					entrys >> size >> name;
-
-					files.emplace_back(new File(size, pwd + '/' + name));
-				}
-			} while (lines[i + 1][0] != '$');
-
-			/*
-			NOTE:
-			dir e
-			29116 f
-			2557 g
-			62596 h.lst
-			 */
+	for (int i = 0; i < f->children.size(); ++i) {
+		if (f->children[i].dir) {
+			// Dir
+			s += sum_size(&f->children[i]);
+			sizes.emplace_back(f->children[i].size);
 		}
 	}
 
-	return 0;
+	return s + ((f->size <= 100000) ? f->size : 0);
 }
 
-uint64_t part2(const char* file) { return 0; }
+void part1() {
+	const auto input = getInput();
+
+	file root{.dir = 1, .size = 0};
+	stack.emplace_back(&root);
+
+	// Lets parse the tree!
+	// Line 1 is always cd /
+	for (int i = 1; i < input.size(); ++i) {
+		// Here we should always be on a cmd line
+		assert(input[i][0] == '$');
+
+		// cd up
+		if (input[i][2] == 'c' && input[i][5] == '.') {
+			stack.pop_back();
+			continue;
+		}
+
+		// We go into a new directory
+		if (input[i][2] == 'c') {
+			stack.back()->children.emplace_back(file{.dir = 1, .size = 0});
+			stack.emplace_back(&stack.back()->children.back());
+			continue;
+		}
+
+		// We add files!
+		assert(input[i][2] == 'l');
+		while (input[++i][0] != '$') {
+			if (input[i][0] == 'd') {
+				continue;
+			}
+
+			// This is a new file
+			uint64_t size = std::stoull(input[i]);
+			// stack.back()->children.emplace_back(file{.dir = 0, .size = size});
+			// Propagate?
+			for (auto f : stack) {
+				f->size += size;
+			}
+
+			if (i == input.size() - 1) {
+				++i;
+				break;
+			}
+		}
+		--i;
+	}
+
+	std::cout << "Part 1: " << sum_size(&root) << std::endl;
+	sizes.emplace_back(root.size);
+}
+
+void part2() {
+	std::ranges::sort(sizes);
+
+	const uint64_t threashold = 30000000 - (70000000 - sizes.back());
+	for (const auto size : sizes) {
+		if (size >= threashold) {
+			std::cout << "Part 2: " << size << '\n';
+			break;
+		}
+	}
+}
+
+int main() {
+	part1();
+	part2();
+}
